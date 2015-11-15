089bb16a76d09d01ddd3865cb2fc2c09a41148e9
Mathew Glodack <glodack@gmail.com>
2015-11-15 13:32:58 -0800

    Create giphy presenter test project
    * Create initial test for building url params
    * Test building url params

---
+++ GiphyPresenter.Tests/Helpers/UrlFormatterTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GiphyPresenter.Helpers;
+using GiphyPresenter.GiphyApi;
+
+namespace GiphyPresenter.Tests.Helpers
+{
+    class UrlFormatterTests
+    {
+        static IEnumerable<Parameter> _Parameters = new Parameter[]
+        {
+            new Parameter() { Name = "something", Value = "Holy Cow" },
+            new Parameter() { Name = "key", Value = "value" },
+        };
+
+        static string expected = "something=Holy Cow&key=value";
+
+        [Test]
+        public void BuildParamString_ShouldReturn_CorrectString()
+        {
+            var result = UrlFormatter.BuildParamString(_Parameters);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ GiphyPresenter/GiphyApi/Parameter.cs
@@ -0,0 +1,8 @@
+﻿namespace GiphyPresenter.GiphyApi
+{
+    public class Parameter
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+}

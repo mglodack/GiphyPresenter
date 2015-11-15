00d65d1ca2e4dcb76c012a8eb19d46b6616506f7
Mathew Glodack <glodack@gmail.com>
2015-11-15 12:35:41 -0800

    Initial commit

---
+++ GiphyPresenter/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GiphyPresenter
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var bootstrapper = new Bootstrapper();
+            bootstrapper.Run();
+        }
+    }
+}
---
+++ GiphyPresenter/Bootstrapper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Practices.Unity;
+using Prism.Mvvm;
+using Prism.Unity;
+using System;
+using System.Globalization;
+using System.Reflection;
+using System.Windows;
+
+namespace GiphyPresenter
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            ViewModelLocationProvider.SetDefaultViewTypeToViewModelTypeResolver((viewType) =>
+            {
+                var viewName = viewType.FullName;
+                viewName = viewName.Replace(".Views.", ".ViewModels.");
+                var viewAssemblyName = viewType.GetTypeInfo().Assembly.FullName;
+                var viewModelName = string.Format(CultureInfo.InvariantCulture, "{0}VM, {1}", viewName, viewAssemblyName);
+                return Type.GetType(viewModelName);
+            });
+
+            ViewModelLocationProvider.SetDefaultViewModelFactory((type) => Container.Resolve(type));
+        }
+    }
+}

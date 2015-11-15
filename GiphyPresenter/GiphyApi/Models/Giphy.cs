d6fa35519fac3228cb4cab3d0275d82d3ec7488e
Mathew Glodack <glodack@gmail.com>
2015-11-15 13:55:39 -0800

    Create deserializable objects for giphy results
    * Create search and random result objects

---
+++ GiphyPresenter/GiphyApi/Models/Giphy.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace GiphyPresenter.GiphyApi.Models
+{
+    public class Giphy
+    {
+        [JsonConstructor]
+        public Giphy()
+        { }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }
+}
---
+++ GiphyPresenter/GiphyApi/Models/Search.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace GiphyPresenter.GiphyApi.Models
+{
+    class Search
+    {
+        [JsonConstructor]
+        public Search() { }
+
+        [JsonProperty("data")]
+        public IEnumerable<Giphy> Results { get; set; }
+    }
+}

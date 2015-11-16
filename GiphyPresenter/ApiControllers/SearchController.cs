d496c427a7cb1b7c71e3db8bea7ff9bed55126ec
Mathew Glodack <glodack@gmail.com>
2015-11-15 18:58:30 -0800

    Create controllers for web access
    * Create basecontroller to hold base params
    * Create search controller for searching gifs

---
+++ GiphyPresenter/ApiControllers/SearchController.cs
@@ -0,0 +1,27 @@
+﻿using GiphyPresenter.GiphyApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiphyPresenter.ApiControllers
+{
+    public class SearchController : BaseController
+    {
+        readonly string _parameter = "q";
+
+        readonly string _endpoint = "search";
+
+        public override string EndPoint
+        {
+            get { return _endpoint; }
+            set { }
+        }
+
+        public Search GetSearchResults(string query)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
